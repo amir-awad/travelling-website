8f74421caa9b9a5c501a739ec38dc37109ebead5
unknown <malekelkassas444@gmail.com>
2022-12-09 00:20:20 +0200

    feat: strating the want to go at paris

--- routes/paris.js
+++ routes/paris.js
@@ -1,8 +1,16 @@
+const { json } = require("body-parser");
 const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
   res.render("paris");
+});
+
+router.post("/", (req, res) => {
+  // res.render("paris");
+  console.log('here');
+  // req.session.wantToGoList.insertone('paris');
+  
 });
 
 module.exports = router;

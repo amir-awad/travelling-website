ac5e2b842d0d0227a3346d93641a9f63530ef4ab
unknown <malekelkassas444@gmail.com>
2022-12-09 23:52:36 +0200

    complting the healper function that update Arrays in MongoDB

--- routes/paris.js
+++ routes/paris.js
@@ -10,7 +10,47 @@ router.post("/", (req, res) => {
   // res.render("paris");
   console.log('here');
   // req.session.wantToGoList.insertone('paris');
-  
+  update(req.session.name);
+  res.render("paris");
 });
+async function update(user_name){
+  const query = { "userName": "12"};
+  const updateDocument = {
+    $push: { "wantToGoList.$[]": "paris" }
+  };
+  console.log(user_name);
+  console.log('done')
+};
+
+const { MongoClient } = require("mongodb");
+// Connection URI
+const uri =
+  "mongodb://0.0.0.0:27017";
+// Create a new MongoClient
+const client = new MongoClient(uri);
+async function run() {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    await client.connect();
+    // Establish and verify connection
+    await client.db("travellingDB").command({ ping: 1 });
+    console.log("Connected successfully to server");
+    await client.db('travellingDB').collection('users').updateOne({userName:'12'},{ 
+      $push: { 
+        wantToGoList: {
+            $each: [ "MAlek" ],
+            $position: 0
+         }
+       } 
+     })  //..insertOne({1:'malek'});
+    var coll = await client.db('travellingDB').collection('users');
+    var tmp = await coll.indexes();
+    console.log(tmp);
+} finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 module.exports = router;

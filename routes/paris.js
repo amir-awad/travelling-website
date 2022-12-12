043cc73e0fd7428c359aef9c33f543d5a0af3ede
unknown <abdelrahman.a.f.m@gmail.com>
2022-12-12 22:29:54 +0200

    feat : no duplicates in the array in mongo

--- routes/paris.js
+++ routes/paris.js
@@ -7,40 +7,39 @@ router.get("/", (req, res) => {
 });
 
 router.post("/", (req, res) => {
+  const username = req.body.username;
   // res.render("paris");
   console.log('here');
-  // req.session.wantToGoList.insertone('paris');
-  update(req.session.name);
+  const { MongoClient } = require("mongodb");
+  // Connection URL
+  const uri = "mongodb://0.0.0.0:27017";
+  // Create a new MongoClient
+  const client = new MongoClient(uri);
+  async function run() {
+    try {
+      // Connect the client to the server (optional starting in v4.7)
+      await client.connect();
+      // Establish and verify connection
+      await client.db("travellingDB").command({ ping: 1 });
+      console.log("Connected successfully to server");
+      var coll = await client.db('travellingDB').collection('users');
+      await coll.updateOne({userName: username  },{ 
+        $addToSet: { 
+          wantToGoList: {
+              $each: ["Paris"],    
+           }
+         } 
+       });
+  } finally {
+      // Ensures that the client will close when you finish/error
+      await client.close();
+    }
+  }
+  run().catch(console.dir);
   res.render("paris");
 });
 
-const { MongoClient } = require("mongodb");
-// Connection URI
-const uri = "mongodb://0.0.0.0:27017";
-// Create a new MongoClient
-const client = new MongoClient(uri);
-async function run() {
-  try {
-    // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
-    // Establish and verify connection
-    await client.db("travellingDB").command({ ping: 1 });
-    console.log("Connected successfully to server");
-    var coll = await client.db('travellingDB').collection('users');
-    await coll.updateOne({userName:'12'},{ 
-      $push: { 
-        wantToGoList: {
-            $each: ["MAlek"],
-            $position: 0
-         }
-       } 
-     });
-} finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+
 
 
 module.exports = router;
